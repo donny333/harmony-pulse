18df0be59c1f6243212cb0a4a4185912e7c10cd9
donny333 <125969039+donny333@users.noreply.github.com>
2023-12-27 17:34:46 +0200

    all trainings added to data.json

--- src/components/pages/dashboards/admin/AdminDashboard.jsx
+++ src/components/pages/dashboards/admin/AdminDashboard.jsx
@@ -51,16 +51,6 @@ const Contacts = () => {
                 </div>);
             })
         }
-        <div className="club">
-            <h1>Laukinė Vyšnia</h1>
-                <div className="trainingInfo">
-                    <h4>Vaikų žaidybinė joga</h4>
-                    <p>Saulė Demidenkaitė</p>
-                    <p>Šeštadienis</p>
-                    <p>12:30 - 14:00</p>
-                    <button>Start Training</button>
-                </div>
-        </div>
     </StyledAdminDashboard>
     );
 }
