d4b92a7001d24c5631cbc582b11004e4e3c018ed
donny333 <125969039+donny333@users.noreply.github.com>
2024-01-17 17:44:16 +0200

    all users are fetched from firebase

--- src/contexts/UsersFBContext.js
+++ src/contexts/UsersFBContext.js
@@ -1,61 +1,22 @@
 import { createContext, useEffect, useState } from "react"
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getFirestore, collection, getDocs, addDoc} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {collection, getDocs} from "firebase/firestore"
+import { database } from "../database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDuF0Vx169rMmf8aDeQiPP9s5GLrqp0SnI",
-  authDomain: "harmony-pulse.firebaseapp.com",
-  projectId: "harmony-pulse",
-  storageBucket: "harmony-pulse.appspot.com",
-  messagingSenderId: "682905692111",
-  appId: "1:682905692111:web:0040c3d3f3747be4bad4ba",
-  measurementId: "G-VV5553DVMH"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app)
-
+let data = [];
 const querySnapshot = await getDocs(collection(database, "users"));
 querySnapshot.forEach((doc) => {
-    console.log(doc.data())
+    data.push(doc.data())
 });
-
-
-
-
 
 // further context
 const UsersFBContext = createContext();
 
 
 const UsersFB = ( { children } ) => {
+    
+    const [usersFB, setUsersFB] = useState(data);
 
-    const [users, setUsers] = useState([]);
-
-    useEffect(()=>{
-        fetch('http://localhost:8080/users')
-            .then(res => res.json())
-            .then(data => setUsers(data))
-    }, [])
-
-
-
-    if(users.length > 0){
-        users.forEach(user => {
-            console.log(user)
-            try {
-                const docRef =addDoc(collection(database, "users"), {...user}) 
-            } catch (e) {
-                console.log("Error adding document: " + e)
-            }
-        })
-    }
+    console.log(usersFB)
 
     return(
         <UsersFBContext.Provider
--- src/database.js
+++ src/database.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app)
+const database = getFirestore(app);
+
+export {database};
