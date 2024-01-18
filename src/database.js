6ddc774a5646dbcaf90d809e5c7f386846ebaeac
donny333 <125969039+donny333@users.noreply.github.com>
2024-01-18 16:21:23 +0200

    minor corrections

--- src/database.js
+++ src/database.js
@@ -6,14 +6,15 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDuF0Vx169rMmf8aDeQiPP9s5GLrqp0SnI",
-  authDomain: "harmony-pulse.firebaseapp.com",
-  projectId: "harmony-pulse",
-  storageBucket: "harmony-pulse.appspot.com",
-  messagingSenderId: "682905692111",
-  appId: "1:682905692111:web:0040c3d3f3747be4bad4ba",
-  measurementId: "G-VV5553DVMH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
